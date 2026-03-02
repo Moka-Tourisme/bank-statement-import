777d47f2d6f681a1ed7cb479c21fc65daf16e43a
DUCIEL Romain <romain@mokatourisme.fr>
2026-03-02 16:45:05 +0100

    [UPD] Update import

--- account_statement_import_tresor_public/models/account_statement_import.py
+++ account_statement_import_tresor_public/models/account_statement_import.py
@@ -1,12 +1,9 @@
 # Copyright 2020 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-import base64
-import logging
-import tempfile
 
-from odoo import _, fields, models
-from odoo.exceptions import UserError
-import csv, shutil
+import logging
+
+from odoo import models
 
 _logger = logging.getLogger(__name__)
 
@@ -14,42 +11,27 @@ _logger = logging.getLogger(__name__)
 class AccountStatementImport(models.TransientModel):
     _inherit = "account.statement.import"
 
-    def _get_default_mapping_id(self):
-        return (
-            self.env["account.journal"]
-            .browse(self.env.context.get("journal_id"))
-            .default_sheet_mapping_id
-        )
-
-    sheet_mapping_id = fields.Many2one(
-        string="Sheet mapping",
-        comodel_name="account.statement.import.sheet.mapping",
-        default=_get_default_mapping_id,
-    )
-    def _parse_file(self, data_file):
-        self.ensure_one()
-        if self.sheet_mapping_id and self.sheet_mapping_id:
-            try:
-                Parser = self.env["account.statement.import.sheet.parser"]
-                print("return data")
-                return Parser.parse(
-                    data_file, self.sheet_mapping_id, self.statement_filename
-                )
-            except BaseException as exc:
-                print('Passage dans le except')
-                if self.env.context.get("account_statement_import_sheet_file_test"):
-                    raise
-                _logger.warning("Sheet parser error", exc_info=True)
-                raise UserError(_("Bad file/mapping: ") + str(exc)) from exc
-        return super()._parse_file(data_file)
-
     def _create_bank_statements(self, stmts_vals, result):
-        """Set balance_end_real if not already provided by the file."""
-        res = super()._create_bank_statements(stmts_vals, result)
-        statements = self.env["account.bank.statement"].browse(result["statement_ids"])
-        for statement in statements:
-            if not statement.balance_end_real:
-                amount = sum(statement.line_ids.mapped("amount"))
-                statement.balance_end_real = statement.balance_start + amount
-        return res
+        """Pre-filter intra-batch duplicates before creating statements.
 
+        The base module only checks unique_import_id against the database.
+        If two identical transactions appear in the same file, they get the
+        same hash/unique_import_id, and both pass the DB check (since
+        neither is created yet). This pre-filter removes such duplicates
+        before the base logic runs.
+        """
+        for st_vals in stmts_vals:
+            seen_ids = set()
+            unique_transactions = []
+            for lvals in st_vals["transactions"]:
+                uid = lvals.get("unique_import_id")
+                if uid and uid in seen_ids:
+                    _logger.info(
+                        "Skipping intra-batch duplicate: %s", uid
+                    )
+                    continue
+                if uid:
+                    seen_ids.add(uid)
+                unique_transactions.append(lvals)
+            st_vals["transactions"] = unique_transactions
+        return super()._create_bank_statements(stmts_vals, result)
--- account_statement_import_tresor_public/wizards/account_statement_import.py
+++ account_statement_import_tresor_public/wizards/account_statement_import.py
@@ -5,9 +5,10 @@ import base64
 import csv
 import hashlib
 import logging
-import shutil
+import os
+import tempfile
 
-from odoo import _, fields, models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 logger = logging.getLogger(__name__)
@@ -22,26 +23,17 @@ class AccountStatementImport(models.TransientModel):
             "statement_ids": [],
             "notifications": [],
         }
-        logger.info("Start to import bank statement file ICI %s", self.statement_filename)
+        logger.info(
+            "Start to import bank statement file %s", self.statement_filename
+        )
 
         file_data = base64.b64decode(self.statement_file)
 
         if self.sheet_mapping_id.is_tresor_public:
-            filename = f'{self.statement_filename}'
-            file = file_data.decode('utf-8')
-            # Save the file in a temp file
-            with open(filename, 'w') as f:
-                f.write(file)
-            # Convert the file
-            self._convert_file(filename)
-            file_data = open(filename, 'rb').read()
-            # Remove all " from the file
-            file_data = file_data.replace(b'"', b'')
-            print("file_data", file_data)
+            file_data = self._convert_tresor_public(file_data)
 
         self.import_single_file(file_data, result)
         logger.debug("result=%s", result)
-        print("result", result)
         if not result["statement_ids"]:
             raise UserError(
                 _(
@@ -52,176 +44,95 @@ class AccountStatementImport(models.TransientModel):
         self.env["ir.attachment"].create(self._prepare_create_attachment(result))
         return result
 
-    def import_single_file(self, file_data, result):
-        print("Ici import single file")
-        parsing_data = self.with_context(active_id=self.ids[0])._parse_file(file_data)
-        print('parsing_data', parsing_data)
-        if not isinstance(parsing_data, list):  # for backward compatibility
-            parsing_data = [parsing_data]
-        logger.info(
-            "Bank statement file %s contains %d accounts",
-            self.statement_filename,
-            len(parsing_data),
-        )
-        i = 0
-        for single_statement_data in parsing_data:
-            print("ICI single_statement_data", single_statement_data)
-            i += 1
-            logger.debug(
-                "account %d: single_statement_data=%s", i, single_statement_data
-            )
-            self.import_single_statement(single_statement_data, result)
+    def _convert_tresor_public(self, file_data):
+        """Convert a Tresor Public CSV file to the standard sheet format.
 
-    def import_single_statement(self, single_statement_data, result):
-        if not isinstance(single_statement_data, tuple):
-            raise UserError(
-                _("The parsing of the statement file returned an invalid result.")
-            )
-        currency_code, account_number, stmts_vals = single_statement_data
-        # Check raw data
-        if not self._check_parsed_data(stmts_vals):
-            return False
-        if not currency_code:
-            raise UserError(_("Missing currency code in the bank statement file."))
-        # account_number can be None (example : QIF)
-        currency = self._match_currency(currency_code)
-        journal = self._match_journal(account_number, currency)
-        if not journal.default_account_id:
-            raise UserError(
-                _("The Bank Accounting Account is not set on the journal '%s'.")
-                % journal.display_name
-            )
-        # Prepare statement data to be used for bank statements creation
-        stmts_vals = self._complete_stmts_vals(stmts_vals, journal, account_number)
-        # Create the bank statements
-        self._create_bank_statements(stmts_vals, result)
-        # Now that the import worked out, set it as the bank_statements_source
-        # of the journal
-        if journal.bank_statements_source != "file_import_oca":
-            # Use sudo() because only 'account.group_account_manager'
-            # has write access on 'account.journal', but 'account.group_account_user'
-            # must be able to import bank statement files
-            journal.sudo().write({"bank_statements_source": "file_import_oca"})
-
-    def _create_bank_statements(self, stmts_vals, result):
-        """Create new bank statements from imported values,
-        filtering out already imported transactions,
-        and return data used by the reconciliation widget"""
-        abs_obj = self.env["account.bank.statement"]
-        absl_obj = self.env["account.bank.statement.line"]
-        print("passage create bank statement")
-        # Filter out already imported transactions and create statements
-        statement_ids = []
-        existing_st_line_ids = {}
-        for st_vals in stmts_vals:
-            st_lines_to_create = []
-            for lvals in st_vals["transactions"]:
-                existing_line = False
-                if lvals.get("unique_import_id"):
-                    existing_line = absl_obj.sudo().search(
-                        [
-                            ("unique_import_id", "=", lvals["unique_import_id"]),
-                        ],
-                        limit=1,
-                    )
-                    # we can only have 1 anyhow because we have a unicity SQL constraint
-                if existing_line:
-                    existing_st_line_ids[existing_line.id] = True
-                    if "balance_start" in st_vals:
-                        st_vals["balance_start"] += float(lvals["amount"])
-                else:
-                    st_lines_to_create.append(lvals)
-
-            if len(st_lines_to_create) > 0:
-                if not st_lines_to_create[0].get("sequence"):
-                    for seq, vals in enumerate(st_lines_to_create, start=1):
-                        vals["sequence"] = seq
-                # Remove values that won't be used to create records
-                st_vals.pop("transactions", None)
-                # Create the statement with lines
-                st_vals["line_ids"] = [[0, False, line] for line in st_lines_to_create]
-                statement = abs_obj.create(st_vals)
-                statement_ids.append(statement.id)
-
-        if not statement_ids:
-            return False
-        result["statement_ids"].extend(statement_ids)
-
-        # Prepare import feedback
-        num_ignored = len(existing_st_line_ids)
-        if num_ignored > 0:
-            if num_ignored == 1:
-                msg = _("1 transaction had already been imported and was ignored.")
-            else:
-                msg = (
-                    _("%d transactions had already been imported and were ignored.")
-                    % num_ignored
-                )
-            result["notifications"].append(msg)
-        statements = self.env["account.bank.statement"].browse(result["statement_ids"])
-        for statement in statements:
-            if not statement.balance_end_real:
-                amount = sum(statement.line_ids.mapped("amount"))
-                statement.balance_end_real = statement.balance_start + amount
-        return statements
-
-    def _convert_file(self, data_file):
-        # print("data_file", data_file)
+        Returns the converted file content as bytes.
+        """
+        decoded = file_data.decode('utf-8')
         dataTransaction = []
-        output_file = "convertFileTemp.csv"
-        with open(data_file, 'r', encoding='utf-8') as csvfile:
-            # Read the CSV file using the semicolon as a delimiter, and data like 0.19 as a float
-            reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE, quotechar='"', doublequote=False)
-            header1, header2 = None, None  # Initialize variables for the headers
+        header1, header2 = None, None
+        header1data, header2data = None, None
+        header3 = None
 
-            # Loop through the CSV file
-            for index, row in enumerate(reader):
-                if index == 3:
-                    # Store data[0] and data[2] as headers and remove the last character
-                    header1 = row[0][1:-3].replace(':', '')
-                    header2 = row[2][1:-3].replace(':', '')
-                    print(header1, header2)
-                elif index == 4:
-                    header1data = row[0][:-1].replace(':', '').replace('"', '')
-                    header2data = row[2][:-1].replace(':', '').replace('"', '')
-                    print(header1data, header2data)
-                elif index == 8:
-                    header3 = row
-                    header3 = [x.replace(' ', '').replace('"', '') for x in header3]
-                    header3.pop()
-                elif index >= 9:
-                    data = row
-                    data = [x.replace('"', '') for x in data]
-                    data.pop()
-                    if data[0] != "":
-                        for index, value in enumerate(data):
-                            if index == 3:
-                                if value == "":
-                                    data[index] = "0,0"
-                                else:
-                                    data[index] = value.replace(' ', '')
-                            elif index == 4:
-                                if value == "":
-                                    data[index] = "0,0"
-                                else:
-                                    data[index] = value.replace(' ', '')
-                        data.insert(0, header1data)
-                        data.insert(1, header2data)
-                        data.insert(2, "EUR")
-                        print("data-ici", data)
-                        # Create an md5 hash of the data
-                        data.append(hashlib.md5(str(data).encode('utf-8')).hexdigest())
-                        dataTransaction.append(data)
+        lines = decoded.splitlines(True)
+        reader = csv.reader(
+            lines,
+            delimiter=';',
+            quoting=csv.QUOTE_NONE,
+            quotechar='"',
+            doublequote=False,
+        )
+
+        for row_idx, row in enumerate(reader):
+            if row_idx == 3:
+                header1 = row[0][1:-3].replace(':', '')
+                header2 = row[2][1:-3].replace(':', '')
+            elif row_idx == 4:
+                header1data = row[0][:-1].replace(':', '').replace('"', '')
+                header2data = row[2][:-1].replace(':', '').replace('"', '')
+            elif row_idx == 8:
+                header3 = [
+                    x.replace(' ', '').replace('"', '') for x in row
+                ]
+                header3.pop()
+            elif row_idx >= 9:
+                data = [x.replace('"', '') for x in row]
+                data.pop()
+                if data[0] != "":
+                    for col_idx, value in enumerate(data):
+                        if col_idx == 3:
+                            data[col_idx] = (
+                                value.replace(' ', '') if value else "0,0"
+                            )
+                        elif col_idx == 4:
+                            data[col_idx] = (
+                                value.replace(' ', '') if value else "0,0"
+                            )
+                    data.insert(0, header1data)
+                    data.insert(1, header2data)
+                    data.insert(2, "EUR")
+                    # Hash only the transaction-specific fields
+                    # (date, valeur, libellé, débit, crédit) to avoid
+                    # header2data trailing spaces changing between exports
+                    hash_data = [x.strip() for x in data]
+                    data.append(
+                        hashlib.md5(
+                            str(hash_data).encode('utf-8')
+                        ).hexdigest()
+                    )
+                    dataTransaction.append(data)
+
         header3.insert(0, header1)
         header3.insert(1, header2)
         header3.insert(2, "Devise")
         header3.append("Id")
 
-        # Write the cleaned data to a new CSV file
-        with open(output_file, mode='w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # Write to a temp file and read back
+        with tempfile.NamedTemporaryFile(
+            mode='w', suffix='.csv', delete=False, newline=''
+        ) as tmp:
+            writer = csv.writer(
+                tmp,
+                delimiter=';',
+                quotechar='"',
+                quoting=csv.QUOTE_MINIMAL,
+            )
             writer.writerow(header3)
             writer.writerows(dataTransaction)
-            shutil.move(output_file, data_file)
+            tmp_path = tmp.name
 
-        return data_file
+        with open(tmp_path, 'rb') as f:
+            converted = f.read()
+
+        os.unlink(tmp_path)
+
+        # Remove all quotes
+        converted = converted.replace(b'"', b'')
+        return converted
+
+    def _convert_file(self, data_file):
+        """Kept for backward compatibility. Use _convert_tresor_public."""
+        return self._convert_tresor_public(
+            open(data_file, 'rb').read()
+        )
